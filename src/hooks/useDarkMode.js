247eba24bdebecd76d1fdef3a47fa4641205f691
JazmineMT <jazmineamcginnis@gmail.com>
2020-07-08 14:05:12 -0700

    MVP

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,14 @@
+import {useEffect} from 'react'
+import {useLocalStorage} from './useLocalStorage'
+
+
+export const useDarkMode = (key, initialValue) => {
+  const [darkMode, setDarkMode] = useLocalStorage(key, initialValue)
+
+  useEffect(() =>{
+    window.document.body.classList.toggle('dark-mode')
+},[setDarkMode])
+
+
+  return [darkMode, setDarkMode]
+}
